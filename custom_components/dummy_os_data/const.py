cfbf87e809b1528c6a67e68940d2feb7173b23ad
Stefan <111599445+bliek79@users.noreply.github.com>
2026-08-26 13:25:30 +0200

    Bump version to alpha 10.3

--- custom_components/dummy_os_data/const.py
+++ custom_components/dummy_os_data/const.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 DOMAIN = "dummy_os_data"
 NAME = "Dummy OS"
-VERSION = "0.1.0-alpha.10.2"
+VERSION = "0.1.0-alpha.10.3"
 
 CONF_HOME_POWER_ENTITY = "home_power_entity"
 DEFAULT_HOME_POWER_ENTITY = "sensor.home_power"
