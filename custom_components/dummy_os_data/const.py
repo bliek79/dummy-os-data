4d2ba9ee8ad2703f86a732b7ec24b7d162978c34
Stefan <111599445+bliek79@users.noreply.github.com>
2026-09-13 11:21:21 +0200

    Alpha 30: restore constants and bump version only

--- custom_components/dummy_os_data/const.py
+++ custom_components/dummy_os_data/const.py
@@ -31,29 +31,10 @@ DATA_POWER_SOURCE_KEYS = [
     CONF_BATTERY_CHARGE_POWER_ENTITY,
     CONF_BATTERY_DISCHARGE_POWER_ENTITY,
 ]
-DEFAULT_GRID_NET_POWER_ENTITY = "sensor.smart_meter_gen_2_grid_connection_power"
-DEFAULT_DATA_SOLAR_POWER_ENTITY = "sensor.solarbank_max_solarbank_solar_power"
-DEFAULT_BATTERY_CHARGE_POWER_ENTITY = "sensor.solarbank_max_solarbank_battery_charging_power"
-DEFAULT_BATTERY_DISCHARGE_POWER_ENTITY = "sensor.solarbank_max_solarbank_battery_discharging_power"
 
-CONF_SOLAR_LATITUDE = "solar_latitude"
-CONF_SOLAR_LONGITUDE = "solar_longitude"
-CONF_SOLAR_NORTH_DC_KWP = "solar_north_dc_kwp"
-CONF_SOLAR_NORTH_AC_KW = "solar_north_ac_kw"
-CONF_SOLAR_NORTH_TILT = "solar_north_tilt"
-CONF_SOLAR_NORTH_AZIMUTH = "solar_north_azimuth"
-CONF_SOLAR_NORTH_FACTOR = "solar_north_factor"
-CONF_SOLAR_SOUTH_DC_KWP = "solar_south_dc_kwp"
-CONF_SOLAR_SOUTH_AC_KW = "solar_south_ac_kw"
-CONF_SOLAR_SOUTH_TILT = "solar_south_tilt"
-CONF_SOLAR_SOUTH_AZIMUTH = "solar_south_azimuth"
-CONF_SOLAR_SOUTH_FACTOR = "solar_south_factor"
-CONF_SOLAR_ACTUAL_NORTH_DC_ENTITY = "solar_actual_north_dc_entity"
-CONF_SOLAR_ACTUAL_SOUTH_DC_ENTITY = "solar_actual_south_dc_entity"
-CONF_SOLAR_ACTUAL_TOTAL_ENTITY = "solar_actual_total_entity"
-DEFAULT_SOLAR_ACTUAL_NORTH_DC_ENTITY = "sensor.solarbank_max_solarbank_solar_power"
-DEFAULT_SOLAR_ACTUAL_SOUTH_DC_ENTITY = "sensor.solarbank_max_solarbank_solar_power"
-DEFAULT_SOLAR_ACTUAL_TOTAL_ENTITY = "sensor.solarbank_max_solarbank_solar_power"
+# Temporary alpha.11.5 option keys retained only to prefill migration safely.
+LEGACY_CONF_GRID_IMPORT_POWER_ENTITY = "grid_import_power_entity"
+LEGACY_CONF_GRID_EXPORT_POWER_ENTITY = "grid_export_power_entity"
 
 CONF_TARIFF_PROFILE_ID = "tariff_profile_id"
 CONF_TARIFF_SUPPLIER = "tariff_supplier"
@@ -71,30 +52,52 @@ CONF_GAS_SUPPLIER = "gas_supplier_incl_vat"
 CONF_GAS_TAX = "gas_tax_incl_vat"
 CONF_GAS_FIXED_SUPPLY_PER_DAY = "gas_fixed_supply_per_day"
 CONF_GAS_GRID_PER_DAY = "gas_grid_per_day"
-DEFAULT_GAS_MARKET_ENTITY = "sensor.current_gas_market_price"
 
-QUARTER_MINUTES = 15
-FORECAST_HORIZON_HOURS = 72
-FORECAST_SLOTS = FORECAST_HORIZON_HOURS * 60 // QUARTER_MINUTES
-MAX_HISTORY_DAYS = 400
-MIN_VALID_COVERAGE = 0.8
-SOLAR_MIN_VALID_COVERAGE = 0.8
+DEFAULT_GAS_MARKET_ENTITY = "sensor.energyzero_today_gas_current_hour_price"
+GAS_VARIABLE_ADDON_ENTITY = "input_number.gas_markup_per_m3"
 
+CONF_SOLAR_ACTUAL_TOTAL_ENTITY = "solar_actual_total_entity"
+CONF_SOLAR_ACTUAL_NORTH_DC_ENTITY = "solar_actual_north_dc_entity"
+CONF_SOLAR_ACTUAL_SOUTH_DC_ENTITY = "solar_actual_south_dc_entity"
+CONF_SOLAR_LATITUDE = "solar_latitude"
+CONF_SOLAR_LONGITUDE = "solar_longitude"
+CONF_SOLAR_NORTH_DC_KWP = "solar_north_dc_kwp"
+CONF_SOLAR_NORTH_AC_KW = "solar_north_ac_kw"
+CONF_SOLAR_NORTH_TILT = "solar_north_tilt"
+CONF_SOLAR_NORTH_AZIMUTH = "solar_north_open_meteo_azimuth"
+CONF_SOLAR_NORTH_FACTOR = "solar_north_performance_factor"
+CONF_SOLAR_SOUTH_DC_KWP = "solar_south_dc_kwp"
+CONF_SOLAR_SOUTH_AC_KW = "solar_south_ac_kw"
+CONF_SOLAR_SOUTH_TILT = "solar_south_tilt"
+CONF_SOLAR_SOUTH_AZIMUTH = "solar_south_open_meteo_azimuth"
+CONF_SOLAR_SOUTH_FACTOR = "solar_south_performance_factor"
+
+DEFAULT_SOLAR_ACTUAL_TOTAL_ENTITY = "sensor.sb3_6_1av_41_857_pv_power"
+DEFAULT_SOLAR_ACTUAL_NORTH_DC_ENTITY = "sensor.sb3_6_1av_41_857_pv_power_a"
+DEFAULT_SOLAR_ACTUAL_SOUTH_DC_ENTITY = "sensor.sb3_6_1av_41_857_pv_power_b"
+
+PLATFORMS = ["sensor", "select", "binary_sensor", "switch", "datetime", "number"]
+
+STORAGE_VERSION = 1
+STORAGE_KEY = f"{DOMAIN}.home_forecast"
+ENERGY_STORE_SCHEMA_VERSION = 2
+ENERGY_EVALUATION_SCHEMA_VERSION = 1
+SOLAR_STORAGE_VERSION = 1
+SOLAR_STORAGE_KEY = f"{DOMAIN}.solar_evaluation"
+
+PROFILE_CONTRACT_VERSION = 1
 PROFILE_NORMAL = "normal"
 PROFILE_AWAY = "away"
 PROFILE_UNCLASSIFIED = "unclassified"
 PROFILE_MIXED = "mixed"
 PROFILE_LEARNING_OPTIONS = [PROFILE_NORMAL, PROFILE_AWAY]
 PROFILE_OPTIONS = [PROFILE_NORMAL, PROFILE_AWAY, PROFILE_UNCLASSIFIED]
-PROFILE_CONTRACT_VERSION = 1
-ENERGY_STORE_SCHEMA_VERSION = 2
-ENERGY_EVALUATION_SCHEMA_VERSION = 1
 
-STORAGE_VERSION = 1
-STORAGE_KEY = "dummy_os_data.home_history"
-SOLAR_STORAGE_VERSION = 1
-SOLAR_STORAGE_KEY = "dummy_os_data.solar"
-
-RECENCY_HALF_LIFE_DAYS = 28.0
-
-PLATFORMS = ["sensor", "select", "binary_sensor", "switch", "datetime", "number"]
+QUARTER_MINUTES = 15
+QUARTER_SECONDS = QUARTER_MINUTES * 60
+QUARTERS_PER_DAY = 96
+FORECAST_HORIZON_HOURS = 72
+FORECAST_SLOTS = FORECAST_HORIZON_HOURS * 60 // QUARTER_MINUTES
+MAX_HISTORY_DAYS = 400
+MIN_VALID_COVERAGE = 0.90
+SOLAR_MIN_VALID_COVERAGE = 0.90
